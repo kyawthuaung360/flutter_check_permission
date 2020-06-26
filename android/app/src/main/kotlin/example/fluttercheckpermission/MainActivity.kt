dc350707c5e11e216051fbbeb941d88e126e5e49
unknown <kanalupar@gmail.com>
2020-06-26 10:29:40 +0630

    first commit

---
+++ android/app/src/main/kotlin/example/fluttercheckpermission/MainActivity.kt
@@ -0,0 +1,6 @@
+package example.fluttercheckpermission
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,174 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () async {
+                var hasOpened = openAppSettings();
+                debugPrint('App Settings opened: ' + hasOpened.toString());
+              },
+            )
+          ],
+        ),
+        body: Column(
+          children: <Widget>[
+            RaisedButton(child: Text('access camera'),onPressed: (){
+                requestPermission(Permission.camera);
+            },),
+            Expanded(
+              child: ListView(
+                  children: Permission.values
+                      .where((Permission permission) {
+                        if (Platform.isIOS) {
+                          return permission != Permission.unknown &&
+                              permission != Permission.sms &&
+                              //permission != Permission.storage &&
+                              permission != Permission.ignoreBatteryOptimizations &&
+                              permission != Permission.accessMediaLocation;
+                        } else {
+                          return permission != Permission.unknown &&
+                              permission != Permission.mediaLibrary &&
+                              permission != Permission.photos &&
+                              permission != Permission.reminders;
+                        }
+                      })
+                      .map((permission) => PermissionWidget(permission))
+                      .toList()), // This trailing comma makes auto-formatting nicer for build methods.
+            ),
+          ],
+        ));
+  }
+
+  Future<void> requestPermission(Permission permission) async {
+    final status = await permission.request();
+
+    setState(() {
+      print(status);
+//      _permissionStatus = status;
+//      print(_permissionStatus);
+    });
+  }
+}
+
+class PermissionWidget extends StatefulWidget {
+  /// Constructs a [PermissionWidget] for the supplied [Permission].
+  const PermissionWidget(this._permission);
+
+  final Permission _permission;
+
+  @override
+  _PermissionState createState() => _PermissionState(_permission);
+}
+
+class _PermissionState extends State<PermissionWidget> {
+  _PermissionState(this._permission);
+
+  final Permission _permission;
+  PermissionStatus _permissionStatus = PermissionStatus.undetermined;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _listenForPermissionStatus();
+  }
+
+  void _listenForPermissionStatus() async {
+    final status = await _permission.status;
+    setState(() => _permissionStatus = status);
+  }
+
+  Color getPermissionColor() {
+    switch (_permissionStatus) {
+      case PermissionStatus.denied:
+        return Colors.red;
+      case PermissionStatus.granted:
+        return Colors.green;
+      default:
+        return Colors.grey;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(_permission.toString()),
+      subtitle: Text(
+        _permissionStatus.toString(),
+        style: TextStyle(color: getPermissionColor()),
+      ),
+      trailing: IconButton(
+          icon: const Icon(Icons.info),
+          onPressed: () {
+            checkServiceStatus(context, _permission);
+          }),
+      onTap: () {
+        requestPermission(_permission);
+      },
+    );
+  }
+
+  void checkServiceStatus(BuildContext context, Permission permission) async {
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: Text((await permission.status).toString()),
+    ));
+  }
+
+  Future<void> requestPermission(Permission permission) async {
+    final status = await permission.request();
+
+    setState(() {
+      print(status);
+      _permissionStatus = status;
+      print(_permissionStatus);
+    });
+  }
+}
